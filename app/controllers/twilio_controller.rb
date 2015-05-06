17c01a22317a44989a85c86f8413ec082e7972d7
Wyncode <wyncode@manuelscbookpro.miamistar.net>
2015-05-06 16:23:18 -0400

    created sms.twiml

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -3,4 +3,12 @@ class TwilioController < ApplicationController
   
   def voice
   end
+  
+  def sms
+    sender = params[:From]
+    body = params[:Body]
+    @chirp = Chirp.new(from: sender, description: body)
+    @chirp.save
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150505054432) do
+ActiveRecord::Schema.define(version: 20150506195127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20150505054432) do
     t.string   "url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "feeds", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
