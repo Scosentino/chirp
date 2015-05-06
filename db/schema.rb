634ba81f2f0c4acbe0facfef6f9a275536e4659a
Wyncode <wyncode@manuelscbookpro.miamistar.net>
2015-05-06 13:27:49 -0400

    update routes to chirps#index

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150505054432) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "chirps", force: :cascade do |t|
     t.string   "from"
     t.text     "description"
